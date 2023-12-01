685ada174fe66cf69e1e24ec7e0c7e98271e12fd
DeveshDighe <deveshdighe30@gmail.com>
2023-12-01 23:23:53 +0530

    update

--- projects/drop Down/style.js
+++ projects/drop Down/style.js
@@ -1,14 +1,25 @@
 var element1 = document.getElementById('UlId'); 
-
 function drop(){
     element1.classList.toggle('newClass');
+    let f = confirm('are you 18');
+    if(f){
+        alert('you are allowed')
+    }
+    else{
+        alert('you are not allowed')
+    }
 }
-
-
 var s = document.getElementById('f');
-
 function hekko(){
     s.innerHTML = '<h1> hello its hekko function <h1>'
 }
 
 
+
+
+
+// // for in function for objects
+// var obj = { name: 'devesh', age: '20', salary: '100000000' };
+// for (key in obj) {
+//     console.log('The ' + key + 'of employee is ' + obj[key]);
+// }
